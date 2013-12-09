e5d892d48352a1a7c967d44367afb4b35df6ae86
Peter Hillerström <peter.hillerstrom@gmail.com>
2013-12-09 18:52:59 +0200

    Add lab.py to easily import everything for experimenting.

---
+++ sagitta/lab.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ast
+import dis
+from . import inspect
+
+from sagitta.arrow import *
+from sagitta.cat import *
+from sagitta.inspect import *
+from sagitta.typevar import *
